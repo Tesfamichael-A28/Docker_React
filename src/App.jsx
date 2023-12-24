86ab47967db9fba1bfbddae4719fa92f2ce8e435
Tesfamichael.A <tesfa722@gmail.com>
2023-12-24 20:27:20 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <>
       <div className="flex flex-row justify-center h-screen items-center">
-        <h1 className="font-bold text-blue-500 text-center text-5xl ">
-          Hello Docker Verison 2.0
+        <h1 className="font-bold text-yellow-500 text-center text-7xl ">
+          Hello Docker Verison 3.0
         </h1>
       </div>
     </>
